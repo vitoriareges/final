598f9d3dd720a3c4d0800d34d407f31fe0fce694
vitoriareges <vitoriareges@hotmail.com>
2020-08-04 18:12:05 +0000

    testing features

--- SD-CRUD/src/app/app-routing.module.ts
+++ SD-CRUD/src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { LoginLayoutComponent } from './layout/login-layout/login-layout.component';
+import { AuthGuard } from './usuarios/shared/auth.guard';
+import { CriarContaComponent } from './usuarios/criar-conta/criar-conta.component';
+import { LoginComponent } from './usuarios/login/login.component';
+import { EsqueciSenhaComponent } from './usuarios/esqueci-senha/esqueci-senha.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
-  }
+    component: HomeLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: '',
+    component: LoginLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'criar-conta', component: CriarContaComponent },
+      { path: 'esqueci-senha', component: EsqueciSenhaComponent }
+    ]
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
